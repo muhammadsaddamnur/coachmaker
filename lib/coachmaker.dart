c1524fbf53661ceab42978dded4615a6778b6803
muhammadsaddamnur <muhammadsaddamnur@gmail.com>
2021-09-01 12:49:26 +0700

    udah oke

--- lib/coachmaker.dart
+++ lib/coachmaker.dart
@@ -1,12 +1,12 @@
 library coachmaker;
 
 export './src/widgets/widget_point.dart';
-import 'package:coachmaker/src/widgets/widget_card.dart';
+import 'package:coachmaker/src/widgets/widget_main.dart';
 import 'package:flutter/material.dart';
 
-import 'src/widgets/widget_container.dart';
-
 enum CoachMakerControl { none, next, close }
+
+class 
 
 class CoachMaker {
   final List<String> initialList;
@@ -25,73 +25,25 @@ class CoachMaker {
 
   OverlayEntry _buildOverlay() {
     return OverlayEntry(builder: (context) {
-      return Stack(
-        fit: StackFit.expand,
-        children: [
-          ColorFiltered(
-            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.8),
-                BlendMode.srcOut), // This one will create the magic
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    // removeOverlay();
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.black,
-                        backgroundBlendMode: BlendMode
-                            .dstOut), // This one will handle background + difference out
-                  ),
-                ),
-                // Positioned(
-                //   left: x - 8,
-                //   top: y - 8,
-                //   child: Container(
-                //     height: h + 16,
-                //     width: w + 16,
-                //     decoration: BoxDecoration(
-                //       color: Colors.red,
-                //     ),
-                //   ),
-                // ),
-                WidgetContainer(
-                  x: x - 8,
-                  y: y - 8,
-                  height: h + 16,
-                  width: w + 16,
-                  padding: 10,
-                  onTap: () {
-                    switch (pointOnTap) {
-                      case CoachMakerControl.next:
-                        nextOverlay();
-                        break;
-                      case CoachMakerControl.close:
-                        removeOverlay();
-                        break;
-                      case CoachMakerControl.none:
-                        break;
-                      default:
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-          // Positioned(
-          //   left: x,
-          //   top: y + h,
-          //   child: Card(
-          //     child: Text(
-          //       'Overlay',
-          //     ),
-          //   ),
-          // ),
-          WidgetCard(
-            top: y + h + 24,
-          )
-        ],
+      return WidgetMain(
+        x: x - 8,
+        y: y - 8,
+        h: h + 16,
+        w: w + 16,
+        padding: 10,
+        onTapPoint: () {
+          switch (pointOnTap) {
+            case CoachMakerControl.next:
+              nextOverlay();
+              break;
+            case CoachMakerControl.close:
+              removeOverlay();
+              break;
+            case CoachMakerControl.none:
+              break;
+            default:
+          }
+        },
       );
     });
   }
